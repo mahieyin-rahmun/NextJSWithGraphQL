d9ed0c2e5ab9f903c5df12b54a75652846341e20
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-06-23 12:06:39 +0600

    (client) initial commit

---
+++ pages/api/graphql/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-micro";
+import type { PageConfig } from "next";
+import { buildSchema } from "type-graphql";
+import { HelloWorldResolver } from "../../../lib/serverless/graphql/resolvers/HelloWorldResolver";
+import { Connection, createConnection, getConnection } from "typeorm";
+import { User } from "../../../lib/serverless/entities/User";
+import { UserResolver } from "../../../lib/serverless/graphql/resolvers/UserResolver";
+import { prepareConnection } from "../../../lib/utils/db";
+
+// disable next js from handling this route
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  schema: await buildSchema({
+    resolvers: [HelloWorldResolver, UserResolver],
+  }),
+  context: async ({ req, res, connection }) => {
+    let databaseConnection = await prepareConnection();
+    return {
+      req,
+      res,
+      connection,
+      databaseConnection,
+    };
+  },
+});
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
